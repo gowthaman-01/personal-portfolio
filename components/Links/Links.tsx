573cef1584a9a7314bc2598c45717795dd4e6267
Gowthaman Aravindan <gowthaman169@gmail.com>
2022-05-30 10:50:24 +0800

    Fix deployment issues

--- components/Links/Links.tsx
+++
@@ -1,7 +0,0 @@
-import { useWindowSize } from "usehooks-ts";
-import Desktop from "./desktop";
-
-export const Links = () => {
-  const { width } = useWindowSize();
-  return <>{width > 1280 && <Desktop />}</>;
-};

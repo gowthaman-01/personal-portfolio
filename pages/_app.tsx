8794ceb72b75e281609c8f3650c48aa752da95e7
Gowthaman Aravindan <gowthaman169@gmail.com>
2022-06-03 11:37:50 +0800

    OG fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Gowthaman</title>
+        <meta property="og:title" content="gowthaman.app" />
+        <meta property="og:image" content="https://i.imgur.com/JcxBZNI.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;

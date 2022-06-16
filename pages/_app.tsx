692893ed4fa9d5e8399d8923a9073afedda7806a
Gowthaman Aravindan <gowthaman169@gmail.com>
2022-06-16 15:32:16 +0800

    Added meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Gowthaman Aravindan</title>
         <meta property="og:title" content="Gowthaman Aravindan" />
         <meta property="og:image" content="https://i.imgur.com/JcxBZNI.png" />
+        <meta
+          property="og:description"
+          content="I am a penultimate engineering student at NUS currently interning at a Web3 startup as a Software Engineer Intern! 
+          Here is my personal webstie - you can view my past projects I have worked on as well as my experiences in different companies 
+          and organisations in the field of software development."
+        />
       </Head>
       <Component {...pageProps} />
     </>

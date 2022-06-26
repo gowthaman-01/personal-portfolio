1ce2e11984013f21c584d5d852846837d8dac1d5
Gowthaman Aravindan <gowthaman169@gmail.com>
2022-06-26 17:52:24 +0800

    Update traiflix link

--- components/Projects/data.ts
+++ components/Projects/data.ts
@@ -1,70 +1,74 @@
 export const data = [
-    {
-        title: "Secand", 
-        image: "/secand.png",
-        link: "https://github.com/gowthaman-01/secand",
-        description: "E-commerce platform to sell second-hand items, with features such as cart, product search, checkout, and payment.",
-        tech: ["MongoDB", "ExpressJS", "ReactJS", "NodeJS", "Redux", "PayPal SDK"],
-    },
-    {
-        title: "Traiflix", 
-        image: "/traiflix.png",
-        link: "https://traiflix.vercel.app/",
-        description: "Trailer streaming website for movies and shows using the TMBD API with payment and user subscription features.",
-        remarks: " Email: demo@gmail.com | Password: demo1234",
-        tech: ["TypeScript", "ReactJS", "TailwindCSS", "StripeSDK"],
-    },
-    {
-        title: "Quizot", 
-        image: "/quizot.png",
-        link: "https://core.telegram.org/bots",
-        description: "Telegram bot that I made for my girlfriend when we were in a long distance relationship :)",
-        tech: ["Python", "Telegram API", "AWS"],
-    },
-    {
-        title: "TesLah", 
-        image: "/teslah.png",
-        link: "https://teslah.vercel.app/",
-        description: "A self-driving car simulation built by implementing driving mechanics, environment, sensors, and collision detection.",
-        tech: ["Machine learning", "JavaScript", "Nerual Networks", "HTML" , "CSS"],
-    },
-    {
-        title: "Imagify", 
-        image: "/MEME.png",
-        link: "https://meme-generator-ten-sable.vercel.app/",
-        description: "Engine that generates meme images from the ImgFlip API and allows users to type top and bottom text captions.",
-        tech: ["ReactJS", "JavaScript", "HTML" , "CSS"],
-    },
-    {
-        title: "Github Finder", 
-        image: "/search.png",
-        link: "https://github-finder-sigma-ivory.vercel.app/",
-        description: "A tool to search and fetch github usernames and repositories",
-        tech: ["JavaScript", "HTML" , "CSS"],
-    },
-    {
-        title: "Reater", 
-        image: "/reater.png",
-        link: "https://reater.vercel.app/",
-        description: "A chrome extension that helps you keep track of websites that you have read halfway in order to prevent tab clutter. This extension is helpful for students who are doing research and want a tool to save websites that they want to earn at another time.",
-        tech: ["JavaScript", "Chrome Extension"],
-    },
-    {
-        title: "Medusa", 
-        image: "/medusa.png",
-        link: "https://github.com/gowthaman-01/medusa",
-        description: "Classic snake game which allows users to control the snake using keyboard inputs",
-        tech: ["Pygame", "Python"],
-    },
-    {
-        title: "A.N.T", 
-        image: "/ant.jpg",
-        link: "/ant.pdf",
-        description: "Built an autonomous robot that navigates through a maze and fires at an infrared target using a flywheel based shooter. Utilized ROS and implemented python scripts in a Linux environment to program the firing mechanism, achieving a 90% accuracy.",
-        tech: ["C++", "Python", "ROS"],
-    },
-
-
-
-    
-]
+  {
+    title: "Secand",
+    image: "/secand.png",
+    link: "https://github.com/gowthaman-01/secand",
+    description:
+      "E-commerce platform to sell second-hand items, with features such as cart, product search, checkout, and payment.",
+    tech: ["MongoDB", "ExpressJS", "ReactJS", "NodeJS", "Redux", "PayPal SDK"],
+  },
+  {
+    title: "Traiflix",
+    image: "/traiflix.png",
+    link: "traiflix-git-main-gowthaman-01.vercel.app",
+    description:
+      "Trailer streaming website for movies and shows using the TMBD API with payment and user subscription features.",
+    remarks: " Email: demo@gmail.com | Password: demo1234",
+    tech: ["TypeScript", "ReactJS", "TailwindCSS", "StripeSDK"],
+  },
+  {
+    title: "Quizot",
+    image: "/quizot.png",
+    link: "https://core.telegram.org/bots",
+    description:
+      "Telegram bot that I made for my girlfriend when we were in a long distance relationship :)",
+    tech: ["Python", "Telegram API", "AWS"],
+  },
+  {
+    title: "TesLah",
+    image: "/teslah.png",
+    link: "https://teslah.vercel.app/",
+    description:
+      "A self-driving car simulation built by implementing driving mechanics, environment, sensors, and collision detection.",
+    tech: ["Machine learning", "JavaScript", "Nerual Networks", "HTML", "CSS"],
+  },
+  {
+    title: "Imagify",
+    image: "/MEME.png",
+    link: "https://meme-generator-ten-sable.vercel.app/",
+    description:
+      "Engine that generates meme images from the ImgFlip API and allows users to type top and bottom text captions.",
+    tech: ["ReactJS", "JavaScript", "HTML", "CSS"],
+  },
+  {
+    title: "Github Finder",
+    image: "/search.png",
+    link: "https://github-finder-sigma-ivory.vercel.app/",
+    description: "A tool to search and fetch github usernames and repositories",
+    tech: ["JavaScript", "HTML", "CSS"],
+  },
+  {
+    title: "Reater",
+    image: "/reater.png",
+    link: "https://reater.vercel.app/",
+    description:
+      "A chrome extension that helps you keep track of websites that you have read halfway in order to prevent tab clutter. This extension is helpful for students who are doing research and want a tool to save websites that they want to earn at another time.",
+    tech: ["JavaScript", "Chrome Extension"],
+  },
+  {
+    title: "Medusa",
+    image: "/medusa.png",
+    link: "https://github.com/gowthaman-01/medusa",
+    description:
+      "Classic snake game which allows users to control the snake using keyboard inputs",
+    tech: ["Pygame", "Python"],
+  },
+  {
+    title: "A.N.T",
+    image: "/ant.jpg",
+    link: "/ant.pdf",
+    description:
+      "Built an autonomous robot that navigates through a maze and fires at an infrared target using a flywheel based shooter. Utilized ROS and implemented python scripts in a Linux environment to program the firing mechanism, achieving a 90% accuracy.",
+    tech: ["C++", "Python", "ROS"],
+  },
+];

a025a2ee55a803c092b92a1992e0ef04631fd849
Gowthaman <gowthaman.aravindan@zendesk.com>
2023-03-09 11:16:03 +0800

    Update chatty png and modify animations

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -20,7 +20,7 @@ const Projects = () => {
             </h2>
           </Fade>
         ) : (
-          <Fade delay={180} direction="up" triggerOnce>
+          <Fade delay={400} triggerOnce>
             <h2 className="secondary-title" id={width < 1028 ? "projects" : ""}>
               My Projects
             </h2>

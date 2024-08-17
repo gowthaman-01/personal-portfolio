d33fd4d217452a46a1017fff33532bd457e9951b
Gowthaman <gowthaman169@gmail.com>
2024-08-17 08:59:54 +0800

    Remove meta tags from _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="Gowthaman Aravindan" />
-        <meta property="og:image" content="https://i.imgur.com/JcxBZNI.png" />
-        <meta
-          property="og:description"
-          content="Check out my personal website to explore innovative software projects and dive into my professional journey with top tech companies"
-        />
-        <meta property="og:url" content="https://gowthaman.app" />
-        <meta property="og:type" content="website" />
         <link rel="image_src" href="https://i.imgur.com/JcxBZNI.png" />
         <link rel="icon" type="image/x-icon" href="/favicon.png" />
         <link
